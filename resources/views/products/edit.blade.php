188b18b1c4b1990886620737bde47a3bcdee640a
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-05-03 15:36:27 +0530

    app

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -49,7 +49,7 @@
 
                         <div class="mb-4">
                             <label class="form-label" for="description">Description <span class="text-danger">*</span></label>
-                            <textarea class="form-control" id="descrip" name="description">{{ old('description', $product->description) }}</textarea>
+                            <textarea class="form-control" id="description" name="description">{{ old('description', $product->description) }}</textarea>
                         </div>
 
                         <div class="mb-4">
