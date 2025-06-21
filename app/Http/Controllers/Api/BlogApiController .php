0c92fd9612625e59cecc7679f97b90bee1be2105
dreamstudio-satheesh <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-06-21 18:55:31 +0530

    app

---
+++ app/Http/Controllers/Api/BlogApiController .php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BlogApiController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Blog::latest();
+
+        if ($request->has('search')) {
+            $query->where('title', 'like', '%' . $request->search . '%')
+                  ->orWhere('slug', 'like', '%' . $request->search . '%');
+        }
+
+        $blogs = $query->select('id', 'title', 'slug', 'feature_image', 'content', 'created_at')->paginate(10);
+
+        return response()->json([
+            'blogs' => $blogs,
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $blog = Blog::where('slug', $slug)->select('id', 'title', 'slug', 'feature_image', 'content', 'created_at')->firstOrFail();
+
+        return response()->json([
+            'blog' => $blog,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\{
     ProductController as ApiProductController,
+    BlogApiController as ApiBlogController,
     CategoryController as ApiCategoryController,
     SliderController as ApiSliderController,
     ServiceController as ApiServiceController,
@@ -14,32 +15,34 @@ use App\Http\Controllers\Api\{
 
 // API Routes (public or authenticated if needed)
 
-    // Public API routes for Products, Services, Projects, etc.
+// Public API routes for Products, Services, Projects, etc.
 
-    Route::get('categories', [ApiCategoryController::class, 'index']);
+Route::get('categories', [ApiCategoryController::class, 'index']);
 
 
-    Route::get('/sliders', [ApiSliderController::class, 'index']);
+Route::get('/sliders', [ApiSliderController::class, 'index']);
 
-    Route::get('products', [ApiProductController::class, 'index']);
-    Route::get('products/{id}', [ApiProductController::class, 'show']);
-    
-    Route::get('services', [ApiServiceController::class, 'index']);
-    Route::get('services/{id}', [ApiServiceController::class, 'show']);
-    
-    Route::get('projects', [ApiProjectController::class, 'index']);
-    Route::get('projects/{id}', [ApiProjectController::class, 'show']);
-    
-    Route::get('galleries', [ApiGalleryController::class, 'index']);
-    Route::get('galleries/{id}', [ApiGalleryController::class, 'show']);
-    
-    Route::get('awards', [ApiAwardController::class, 'index']);
-    Route::get('awards/{id}', [ApiAwardController::class, 'show']);
-    
-    Route::get('contacts', [ApiContactController::class, 'index']);
-    Route::get('contacts/{id}', [ApiContactController::class, 'show']);
+Route::get('/blogs', [ApiBlogController::class, 'index']);
+Route::get('/blogs/{slug}', [ApiBlogController::class, 'show']);
+
+Route::get('products', [ApiProductController::class, 'index']);
+Route::get('products/{id}', [ApiProductController::class, 'show']);
+
+Route::get('services', [ApiServiceController::class, 'index']);
+Route::get('services/{id}', [ApiServiceController::class, 'show']);
+
+Route::get('projects', [ApiProjectController::class, 'index']);
+Route::get('projects/{id}', [ApiProjectController::class, 'show']);
+
+Route::get('galleries', [ApiGalleryController::class, 'index']);
+Route::get('galleries/{id}', [ApiGalleryController::class, 'show']);
+
+Route::get('awards', [ApiAwardController::class, 'index']);
+Route::get('awards/{id}', [ApiAwardController::class, 'show']);
+
+Route::get('contacts', [ApiContactController::class, 'index']);
+Route::get('contacts/{id}', [ApiContactController::class, 'show']);
     
     // Optionally, you can add POST routes for creating records if needed.
     // Example for adding products:
     // Route::post('products', [ApiProductController::class, 'store'])->middleware('auth:api');
-
